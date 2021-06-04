a2bff5c2f8a8b4b65c55c1fcf0863eaed31e6922
Gigi <mr_gigi@icloud.com>
2021-06-04 16:43:02 +0200

    Redesign of the website.
    Version Beta-0.1
    
    This is not the final website.
    There are a lot of bugs and problems here and there.
    
    But it is good enough to publish it.

---
+++ assets/js/age.js
@@ -0,0 +1,11 @@
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+document.getElementById('age').innerHTML = ('I am ' + getAge("2004/07/14") + ' years old, my birthday is on the 14th of July.');
